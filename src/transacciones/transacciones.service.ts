da7f39fc367a42a10000427337693cbf48187510
joellop <joel_blg@outlook.com>
2025-02-19 12:47:13 -0600

    agregar un comentario en transacciones.service.ts

--- src/transacciones/transacciones.service.ts
+++ src/transacciones/transacciones.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TransaccionesService {
+    //Ejemplo de Dto que usare mas adelante
     private respuestaApi = {
         dato: [
             {
